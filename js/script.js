54c743fac93c3e16dd2b1a82fa2382dca7bfd360
callmejogi <kutafin01@gmail.com>
2021-10-12 16:48:41 +0300

    finallyReady

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 let n = +prompt(`Type a number:`);
 
 nextPrime:
-for (let i = 2;i <= n;i++){
-    
-    for(let j = 2;j < i; j++){
-        if(i % j === 0) {
-            console.log(`${j} = false`);
-        }else{
-            console.log(`${j} = true`);
-        }
-    }
-    
+for (let i = 2; i <= n; i++) { 
 
+  for (let j = 2; j < i; j++) { 
+    if (i % j === 0) {
+        console.log(`false number ${i}`); 
+        continue nextPrime;
+    };
 
-};
+  };
+
+  console.log( `true number ${i}`); 
+}
