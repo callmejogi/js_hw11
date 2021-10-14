7ff936f5952af090270fd899b7971ecbe57f480e
callmejogi <kutafin01@gmail.com>
2021-10-14 18:39:00 +0300

    problemFix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 let n = +prompt(`Type a number:`);
 
 
-for (let i = 2; i <= n; i++) { 
+for (let i = 2; i < n; i++) { 
+
+  //if (n % i === 0) {
+  //  console.log(`false number ${i}`); 
+  //}else{
+  //  console.log( `true number ${i}`);
+  //};
+
 
   if (n % i === 0) {
-    console.log(`false number ${n}`); 
-    break;
-  }else{
-    console.log( `true number ${n}`);
-    break;
+    console.log(`false`);
   };
+
+
+
+console.log( `true`); 
 
 }
