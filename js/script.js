13ce70feab181053a3938d1b7a5720e4a71cb84f
callmejogi <kutafin01@gmail.com>
2021-10-14 19:19:10 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -3,20 +3,14 @@ let n = +prompt(`Type a number:`);
 
 for (let i = 2; i < n; i++) { 
 
-  //if (n % i === 0) {
-  //  console.log(`false number ${i}`); 
-  //}else{
-  //  console.log( `true number ${i}`);
-  //};
-
-
   if (n % i === 0) {
     console.log(`false`);
     break;
+
+
   };
 
-
+};
 
 console.log( `true`); 
 
-}
