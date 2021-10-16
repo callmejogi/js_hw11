b229271181ce062b1342e7af60e01539acf65a84
callmejogi <kutafin01@gmail.com>
2021-10-16 21:42:10 +0300

    workWithMistakes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-let n = +prompt(`Type a number:`);
+const n = +prompt(`Type a number:`);
 
 
-for (let i = 2; i < n; i++) { 
+if(isNaN(n)){
+  alert(`NaN!`);
+}else if(n < 0){
+  alert(`Отрицательное число!`)
+}else if(n === 1 || n === 0){
+  console.log(`false`);
+}else{
+  for (let i = 2; i < n; i++) { 
 
-  if (n % i === 0) {
-    console.log(`false`);
-    break;
-
+    if (n % i === 0) {
+      console.log(`false`);
+      break;
+    };
 
   };
-
-};
+}
 
 console.log( `true`); 
 
